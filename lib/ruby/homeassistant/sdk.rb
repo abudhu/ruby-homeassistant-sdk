82b61010fd36e7eaaefc0532216dc606b6c77c73
abudhu <abudhu@gmail.com>
2025-02-22 08:49:59 -0800

    Fix Init Push

---
+++ lib/ruby/homeassistant/sdk.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "sdk/version"
+
+module Ruby
+  module Homeassistant
+    module Sdk
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
